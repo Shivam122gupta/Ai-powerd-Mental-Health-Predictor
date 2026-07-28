f3691c3315a29b65daff374a429b9d40ee8ef85b
Shivam Gupta <22shivamgupta05@gmail.com>
2026-07-28 20:45:07 +0530

    req3

--- src/utils.py
+++ src/utils.py
@@ -50,7 +50,7 @@ def evaluate_models(xtrain,ytrain,xtest,ytest,models):
 def load_object(file_path):
     try:
         with open(file_path,"rb") as file_obj:
-            return pickle.load(file_obj)
+            return dill.load(file_obj)
         
     except Exception as e:
         raise CustomException(e,sys)
